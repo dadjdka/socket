c10fd3a1cbaa0c0dce9732e57f1f2cc90af617de
zseujmzx123456 <254707268@qq.com>
2020-04-20 22:23:28 +0800

    tcp udp

---
+++ 4.php
@@ -0,0 +1,11 @@
+<?php
+
+$fp = stream_socket_client("udp://127.0.0.1:1113",$errno,$errstr);
+
+if (!$fp){
+    echo "$errno";
+}else{
+    fwrite($fp,"hello word");
+    echo fread($fp,1204);
+    fclose($fp);
+}
